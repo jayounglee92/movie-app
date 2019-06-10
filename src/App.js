644ed1b9f52451c441996a2d8b0e71794ba01cc8
jayounglee92 <lzy6853@gmail.com>
2019-06-10 18:09:48 +0900

    change to class component, lifecycle, state, loading function

--- src/App.js
+++ src/App.js
@@ -1,49 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }from 'react';
+// import logo from './logo.svg';
 import './App.css';
 import Movie from './movie';
 
-const movieTitles = [
-  "알라딘",
-  "기생충",
-  "액스맨-다크 피닉스",
-  "로켓맨"
-]
 
-const movieImages = [
-  "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81478/81478_185.jpg",
-  "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81774/81774_185.jpg",
-  "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81218/81218_185.jpg",
-  "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81847/81847_185.jpg"
-]
+// map을 이용할 때는 index 가 필수
+class App extends Component {
 
-const movies = [
-  {
-    title: "알라딘",
-    poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81478/81478_185.jpg"
-  },
-  {
-    title: "기생충",
-    poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81774/81774_185.jpg"
-  },
-  {
-    title: "액스맨-다크 피닉스",
-    poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81218/81218_185.jpg"
-  },
-  {
-    title: "로켓맨",
-    poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81847/81847_185.jpg"
+  // Render Flow : componentWillMount() => render() => componentDidMount()
+  // Update Flow : componentWillReceiveProps() => shouldComponentUpdate() old props과 new props가 다르면 true 반환함 => render() => compoenentDidMount()
+
+  // state = {
+  //   greeting: 'Hello!',
+  //   movies: [
+  //     {
+  //       title: "알라딘",
+  //       poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81478/81478_185.jpg"
+  //     },
+  //     {
+  //       title: "기생충",
+  //       poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81774/81774_185.jpg"
+  //     },
+  //     {
+  //       title: "액스맨-다크 피닉스",
+  //       poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81218/81218_185.jpg"
+  //     },
+  //     {
+  //       title: "로켓맨",
+  //       poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81847/81847_185.jpg"
+  //     },
+  //     {
+  //       title: '라라랜드',
+  //       poster: 'https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png'
+  //     }
+  //   ]
+  // }
+
+  componentWillMount(){
+    console.log('will mount');
   }
-]
 
-function App() {
-  return (
-    <div className="App">
-      {movies.map((movie, index) => {      
-        return <Movie title={movie.title} poster={movie.poster} key={index}/>
-      })}
-    </div>
-  );
+  state = {
+
+  }
+
+  componentDidMount(){
+    console.log('did mount');
+    
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title: "알라딘",
+            poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81478/81478_185.jpg"
+          },
+          {
+            title: "기생충",
+            poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81774/81774_185.jpg"
+          },
+          {
+            title: "액스맨-다크 피닉스",
+            poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81218/81218_185.jpg"
+          },
+          {
+            title: "로켓맨",
+            poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81847/81847_185.jpg"
+          },
+          {
+            title: '라라랜드',
+            poster: 'https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png'
+          }
+        ]     
+      })
+    }, 5000)
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {      
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+    return movies
+  }
+
+  render(){
+    console.log("did render");
+
+    return (
+      <div className="App">       
+        {this.state.movies ?  this._renderMovies() : 'Loading'}  
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/movie.js
+++ src/movie.js
@@ -1,30 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './movie.css';
 
-function Movie(props) {
+class Movie extends Component {
 
-    // To check type of props
-    let propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
+    static propTypes = {
+             title: PropTypes.string.isRequired,
+             poster: PropTypes.string.isRequired
     }
-    return (        
-        <div>
-            <MoviePoster poster={props.poster}/>
-            <h1>{props.title}</h1>
-        </div>
-    );
+
+    render(){
+        return (        
+            <div>
+                <MoviePoster poster={this.props.poster}/>
+                <h1>{this.props.title}</h1>
+            </div>
+        );
+    }
+    
 }
 
-function MoviePoster(props) {
-    // To check type of props
-    let propTypes = {
+// To check type of props in Functional Component : 타입과 필수인지 아닌 지를 확인하여 체크해줌
+// Movie.propTypes = {
+//     title: PropTypes.string.isRequired,
+//     poster: PropTypes.string.isRequired
+// }
+
+class MoviePoster extends Component {
+    static propTypes = {
         poster: PropTypes.string.isRequired
     }
-    return (
-        <img src={props.poster} alt="Movie Poster"/>
-    ); 
+
+    render(){
+        return (
+            <img src={this.props.poster} alt="Movie Poster"/>
+        );
+    }
+     
 }
 
 export default Movie;
+
+
+/*
+    [ Functional Component VS Class Component ]
+
+    1. Class Component
+        - componentDidMount, getDerivedStateFromProps등의 React Component의 라이프사이클을 정의해서 사용
+        
+    2.  Functional Component
+        - 라이프사이클은 사용할 수 없으며 온전히 render함수만 정의
+        - Class Component에 비해 성능이 좋고 가독성, 테스트가 용이
+        - 말그대로 함수형 컴포넌트이기에 함수형 프로그래밍의 장점을 활용
+        - React 16.8 버전부터 Functional Component에서 state와 라이프사이클을 사용할 수 있는 Hook이라는 개념이 등장
+        - Hook은 state와 componentDidMount, componentDidUpdate, componentWillMount 세 가지 라이프사이클만 구현
+
+    - https://boxfoxs.tistory.com/395
+    - https://medium.com/@minoo/react-stateless-functional-components-%EC%9A%B0%EB%A6%AC%EA%B0%80-%EA%B0%84%EA%B3%BC%ED%95%98%EA%B3%A0-%EC%9E%88%EB%8A%94-9%EA%B0%80%EC%A7%80-ecef2ef73d79
+*/ 
